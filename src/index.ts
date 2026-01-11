52d0e62c187be1aea3dc4679edfd98e2b59cec70
otvkatibe <otaviokatibe@gmail.com>
2026-01-11 15:59:59 -0300

    feat: setup basic express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello World from TypeScript!");
+import express from 'express';
+import { Request, Response } from 'express';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/status', (req: Request, res: Response) => {
+    res.status(200).send({ status: 'server is running' });
+});
+
+app.listen(3000, () => {
+    console.log('App running on port 3000');
+});
